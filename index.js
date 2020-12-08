c5d4d2503c0c05de737465745fa7ed6416816a0b
aleks096575 <69692440+aleks096575@users.noreply.github.com>
2020-12-08 22:37:30 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+var data = [
+	{
+		color: 'rgb(222, 151, 151)',
+		value: 70
+	},
+	{
+		color: 'rgb(151, 154, 222)',
+		value: 20
+	},
+	{
+		color: 'rgb(151, 222, 153)',
+		value: 30
+	},
+	{
+		color: 'rgb(221, 222, 151)',
+		value: 100
+	},
+	{
+		color: 'rgb(222, 201, 151)',
+		value: 90
+	},
+	{
+		color: 'rgb(151, 222, 218)',
+		value: 60
+	},
+	{
+		color: 'rgb(222, 151, 216)',
+		value: 10
+	},
+	{
+		color: 'rgb(176, 151, 222)',
+		value: 35
+	},
+];
+
